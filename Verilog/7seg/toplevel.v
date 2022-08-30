4e517ac8dd5de10cce433bb3dc272c0f09eee1f3
Al <folknology@gmail.com>
2022-08-30 22:14:53 +0100

    7seg still has some bug

--- Verilog/7seg/toplevel.v
+++ Verilog/7seg/toplevel.v
@@ -5,32 +5,6 @@ module toplevel(
   output [11:0] tile3
 );
 
-// # Tile 3
-// set_io -nowarn tile3[0] B1
-// set_io -nowarn  tile3[1] B2
-// set_io -nowarn  tile3[2] C4
-// set_io -nowarn  tile3[3] C3
-// set_io -nowarn  tile3[4] C2
-// set_io -nowarn  tile3[5] C1
-// set_io -nowarn  tile3[6] E1
-// set_io -nowarn  tile3[7] D1
-// set_io -nowarn  tile3[8] D2
-// set_io -nowarn  tile3[9] D3
-// set_io -nowarn  tile3[10] E2
-// set_io -nowarn  tile3[11] E3
-
-// set_io a C1
-// set_io b D1
-// set_io c E3
-// set_io d D3
-// set_io e E1
-// set_io f C2
-// set_io g C3
-// set_io dp D2
-// set_io ca[0] C4
-// set_io ca[1] B2
-// set_io ca[2] B1
-
  reg [36:0] timer;
 
  wire [11:0] val = timer[36:25];
