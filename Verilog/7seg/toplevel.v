a4494e63336fd3bd5cbdc5adb8782d06864d24e1
Al <folknology@gmail.com>
2022-08-31 20:19:54 +0100

    fix the c/d Segments

--- Verilog/7seg/toplevel.v
+++ Verilog/7seg/toplevel.v
@@ -10,9 +10,9 @@ module toplevel(
  wire [11:0] val = timer[36:25];
  wire [2:0] ca;
  wire  a,b,c,d,e,f,g;
- assign tile3 = {c, 'b1, d, 'b1, b, e, a, f, g, ca};
- 
 
+ assign tile3 = {c, 1'b1, d, 1'b1, b, e, a, f, g, ca};
+ 
  always @(posedge clk) timer <= timer + 1;
  
  wire [3:0] dig = (ca == 'b011 ? val[3:0] : ca == 'b101 ? val[7:4] : val[11:8]);
